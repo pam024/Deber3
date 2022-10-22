5ac9207ef2f56ef0c28639ba009d16972ff416d1
Alejandra Ospina <aleospinagracia@hotmail.com>
2022-10-22 04:51:34 -0500

    Manhattan heuristic implementation

--- Proyecto3.py
+++ Proyecto3.py
@@ -1,12 +1,14 @@
+from hashlib import new
 import json
 import math
 from collections import deque
 from copy import deepcopy
+from queue import PriorityQueue
 from timeit import default_timer
 
 # Goal state of the puzzle
 goal = [1, 2, 3, 4, 5, 6, 7, 8, 0]
-puzzle = [2,3,5,1,4,7,0,8,6]
+str_goal = "".join(str(t) for t in goal)
 
 # Calculates the possible moves of the blank tile.
 def get_moves(puzzle):
@@ -34,6 +36,7 @@ def get_moves(puzzle):
     return possible_moves
 
 
+
 # Moves the blank tile in the puzzle.
 def move(puzzle, direction):
     # Creates a copy of the new_puzzle to change it.
@@ -46,23 +49,34 @@ def move(puzzle, direction):
 
     return new_puzzle
 
+def findMove(number):
+    if(number==-1):
+        move_tile = "Left"
+    elif(number==1):
+        move_tile = "Right"
+    elif(number==3):
+        move_tile = "Down"
+    elif(number==-3):
+        move_tile = "Up"
+
+    return move_tile
+
 
 # Creates the database.
-def create_database():
+def createDatabase():
     # Initializes a timer, starting state, queue and visited set.
     begin = default_timer()
-    start = puzzle
+    start = goal
     queue = deque([[start, 0]])
     entries = set()
     visited = set()
-    numNodes = 0
+    entriesM = set()
 
     print("Generating database...")
     print("Collecting entries...")
     # BFS taking into account a state and the cost (number of moves) to reach it from the starting state.
     while queue:
         state_cost = queue.popleft()
-        numNodes +=1
         state = state_cost[0]
         cost = state_cost[1]
 
@@ -79,29 +93,85 @@ def create_database():
             if not "".join(str(t) for t in next_state) in visited:
                 queue.append(next_state_cost)
 
+            manhattanCost = sum(abs(b%3 - g%3) + abs(b//3 - g//3) for b, g in ((state.index(i), goal.index(i)) for i in range(1, 9)))
+
+            
             entries.add(("".join(str(t) for t in state), cost))
+            entriesM.add(("".join(str(t) for t in state), manhattanCost))
+            
             visited.add("".join(str(t) for t in state))
 
-        # Print a progress for every x entries in visited.
-        if len(entries) % 10000 == 0:
-            print("Entries collected: " + str(len(entries)))
-
         # Exit loop when all permutations for the puzzle have been found.
-        if len(entries) >= 181440 or next_state==goal: #9!/2
+        if len(entries) >= 181440*2:
             break
 
     print("Writing entries to database...")
     # Writes entries to the text file, sorted by cost in ascending order .
     with open("database.txt", "w") as f:
+        
         for entry in sorted(entries, key=lambda c: c[1]):
             json.dump(entry, f)
             f.write("\n")
+    
+    dic = {}
+    with open("databaseM.json", "w") as f1:
+        for entry in sorted(entriesM, key=lambda c: c[1]):
+            dic[entry[0]]= entry[1]
+    
+        json.dump(dic, f1)
+    
 
     end = default_timer()
     minutes = math.floor((end-begin)/60)
     seconds = math.floor((end-begin) % 60)
-    print(numNodes)
     return "Generated database in " + str(minutes) + " minute(s) and " + str(seconds) + " second(s)."
 
 
-print(create_database())
+print(createDatabase())
+
+
+
+def searchSolutionManhattan(puzzle):
+    
+    print("Complete board state", puzzle)
+
+    #Read from archive 
+    f2 = open('databaseM.json')
+    js = json.load(f2)
+
+    str_puzzle = "".join(str(t) for t in puzzle)
+    start_cost = js[str_puzzle]
+    node = puzzle
+    frontier = PriorityQueue()
+    frontier.put((start_cost,node))
+    reached = {str_puzzle:start_cost}
+    reached0 = {str_puzzle: 0}
+    reached_path = {str_puzzle: "Start"}
+    num_nodes = 0
+
+    while not frontier.empty():
+        node = frontier.get()[1]
+        str_node = "".join(str(t) for t in node)
+        nodeCost = reached0.get(str_node)
+        
+        if (node == goal):
+            reached_path[str_goal] = reached_path[str_node] + ", End"
+            print ("Number of nodes expanded: ", reached0.__len__(), "\nPath cost: ",reached.get(str_goal), "\nPath: ",reached_path[str_goal])
+        
+        for m in get_moves(node):
+            child = move(node, m)
+            str_child = "".join(str(t) for t in child)
+            child_cost0 = nodeCost + 1
+            child_cost = js[str_child] + child_cost0
+            if not reached.__contains__(str_child) or child_cost < reached.get(str_child):
+                num_nodes+=1
+                reached[str_child] = child_cost
+                reached0[str_child] = child_cost0
+                reached_path[str_child] = reached_path[str_node] + ", " + findMove(m) 
+                frontier.put((child_cost, child))
+
+
+    return "\n"
+
+print ("\nManhattan heuristic")
+print (searchSolutionManhattan([2, 3, 5, 1, 4, 7, 0, 8, 6]))
